80191770f7a66ac2eb3268628a6964099a801a45
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-01-10 11:37:06 +0530

    Create CountWaysDPApproach.java
    https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1/#

---
+++ Amazon/Count ways to N'th Stair(Order does not matter)/CountWaysDPApproach.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    Long countWays(int m)
+    {
+        // your code here
+        long[] dp=new long[m+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=m;i++){
+            dp[i]=1+dp[i-2];
+            //at (i-2)th step, we can have two options..take a 2 step jump to ith step..
+            //or take two 1 step jump to step i..
+            //ek way toh jump(i-2) me hi aa jaayega..
+            //dusra waala..ek extra 1 add karna padega for a 2 step jump..
+        }
+        
+        //we can also see ways(3)=ways(2)..i.e dp[3]=dp[2]..
+        //dp[3]=dp[2]=x1
+        //dp[5]=dp[4]=x1+1=x2
+        //dp[7]=dp[6]=x1+1+1=x2+1=x3
+        //if we initialize dp[0]=dp[1]=1..
+        //we can have the relationship as dp[i]=dp[i-2]+1..
+        //i.e dp[3]=dp[1]+1=dp[2]=dp[0]+1=x1
+        return dp[m];
+        
+    }    
+}
