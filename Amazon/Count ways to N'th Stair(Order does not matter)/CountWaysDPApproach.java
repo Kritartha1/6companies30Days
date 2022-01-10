9f805e407868257d606900ae590196869b8393a6
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-01-10 11:40:19 +0530

    Update CountWaysDPApproach.java

--- Amazon/Count ways to N'th Stair(Order does not matter)/CountWaysDPApproach.java
+++ Amazon/Count ways to N'th Stair(Order does not matter)/CountWaysDPApproach.java
@@ -10,10 +10,14 @@ class Solution
         dp[1]=1;
         for(int i=2;i<=m;i++){
             dp[i]=1+dp[i-2];
+            
             //at (i-2)th step, we can have two options..take a 2 step jump to ith step..
             //or take two 1 step jump to step i..
             //ek way toh jump(i-2) me hi aa jaayega..
             //dusra waala..ek extra 1 add karna padega for a 2 step jump..
+            //we are not doing dp[i-1]+dp[i-2] bcoz ordering doesn't matter..
+            //so reaching dp[i-2] will give one way to rech dp[i]..if we take 1 step at a time
+            //then adding 1 to it gives the other way taking a 2 step to reach i th step
         }
         
         //we can also see ways(3)=ways(2)..i.e dp[3]=dp[2]..
