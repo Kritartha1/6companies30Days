e3aba0afbba721833fad730858e63370972a5e01
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-01-28 22:00:51 +0530

    Create ALexAlwaysWinsSimpleMath.java

---
+++ Walmart/Stone Game/ALexAlwaysWinsSimpleMath.java
@@ -0,0 +1,14 @@
+class Solution {
+  //leetcode.com/problems/stone-game/submissions/
+    
+    public boolean stoneGame(int[] piles) {
+        
+        return true;
+        //Alex will always win no matter what the condition is..bcoz he has the first move...LOL
+        
+        
+    }
+    
+   
+    
+}
